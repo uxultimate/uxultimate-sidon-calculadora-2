d84fcea2fed82426897667d6ce10fc3cc1977d0a
Alberto Mañueco González <alber.m.gonz@gmail.com>
2025-08-05 16:24:46 +0000

    The build is failing, prapared the app to be build and publish!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type {Metadata} from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
-import { Logo } from '@/components/ui/logo';
+import { Logo } from '@/components/logo';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
