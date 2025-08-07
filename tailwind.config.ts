24b13355778b29b8c0a86e807709cbef80d52a67
Alberto Mañueco González <alber.m.gonz@gmail.com>
2025-08-07 09:37:04 +0000

    Try fixing the dropdown-menu, check if the component has the full css ne

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -76,10 +76,32 @@ module.exports = {
             height: '0',
           },
         },
+        "content-show": {
+            from: {
+              opacity: "0",
+              transform: "translate(-50%, -48%) scale(0.96)",
+            },
+            to: {
+              opacity: "1",
+              transform: "translate(-50%, -50%) scale(1)",
+            },
+          },
+          "content-hide": {
+            from: {
+              opacity: "1",
+              transform: "translate(-50%, -50%) scale(1)",
+            },
+            to: {
+              opacity: "0",
+              transform: "translate(-50%, -48%) scale(0.96)",
+            },
+          },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        "content-show": "content-show 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        "content-hide": "content-hide 150ms cubic-bezier(0.16, 1, 0.3, 1)",
       },
     },
   },
